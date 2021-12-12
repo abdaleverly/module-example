08ba41f2a74d78194d208ae09f1ee7c023d05123
abdal <abdal.garuba@gmail.com>
2021-12-11 21:00:30 -0600

    more module examples

--- main.tf
+++ main.tf
@@ -1,19 +1,11 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners = ["099720109477"] # Canonical
+module "happy" {
+  count = 2
+  source = "./modules"
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+  name = "name-${count.index + 1}"
 }
 
-resource "aws_instance" "web" {
-  ami = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+module "sad" {
+  source = "./module2"
+  
 }
